18be8096d4cf934951309ae71c6c8c8d801304f4
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2022-06-04 15:55:04 +0900

    Add platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CustomRepeatDate",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "CustomRepeatDate",

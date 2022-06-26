5bef6ec3cbd2f09ed8f9db5c02cd86bf0fb856ce
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2022-06-26 20:38:52 +0200

    Make CustomRepeatDateOption Hashable

--- Sources/CustomRepeatDate/CustomRepeatDateOption.swift
+++ Sources/CustomRepeatDate/CustomRepeatDateOption.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum CustomRepeatDateOption: Codable {
+public enum CustomRepeatDateOption: Codable, Hashable {
     /// Event will occur every X day(s). The frequency can be 1 ~ 999.
     case daily(frequency: Int)
     /// Event will occur every X week(s) on the weekdays. The frequency can be 1 ~ 999.
@@ -10,14 +10,14 @@ public enum CustomRepeatDateOption: Codable {
     /// Event will occur every X year(s) which matches a given option. The frequency can be 1 ~ 999.
     case yearly(frequency: Int, option: YearlyOption)
 
-    public enum MonthlyOption: Codable {
+    public enum MonthlyOption: Codable, Hashable {
         /// Days of a month, such as on the 25th and 31st.
         case daysOfMonth(days: [Int])
         /// Days of a  week, such as on the 3rd Tuesday.
         case daysOfWeek(weekdayOrdinal: WeekdayOrdinal, weekday: Weekday)
     }
 
-    public enum YearlyOption: Codable {
+    public enum YearlyOption: Codable, Hashable {
         /// Days of a year in specified months, such as on the 25th and 31st of April and August.
         case daysOfYear(months: [Int])
         /// Days of a  week in specified months, such as on the 3rd Tuesday of April and Auguest.
